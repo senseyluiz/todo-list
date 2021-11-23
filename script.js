da8d6ee71f75c099d140b6e439d3426ca35f7622
Luis Henrique <senseyluiz@gmail.com>
2021-11-23 14:55:15 -0300

    Adicionando requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let lista = document.querySelector("#lista-tarefas")
-
 let criaTarefa = document.querySelector("#criar-tarefa");
 let inputTarefa = document.querySelector("#texto-tarefa");
 
+
+//Criando as tarefas para ser adicionadas na lista de tarefas
 criaTarefa.addEventListener("click", function() {
     let tarefa = document.createElement("li");
     tarefa.innerText = inputTarefa.value;
     inputTarefa.value = "";
     lista.appendChild(tarefa);
     console.log(lista);
+})
+
+lista.addEventListener("click", function(e) {
+    e.target.style.backgroundColor = "rgb(128, 128, 128)"
 })
